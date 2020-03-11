9d6587089f858c66c497af80594c9c21c9233264
Natnael Getahun <connect@ngetahun.me>
2020-03-11 18:09:01 +0100

    Update README and main.js

--- README.md
+++ README.md
@@ -1,31 +1,35 @@
-# client
+# Graph Editor View
 ## Yarn installation
-``` npm i -g yarn```
+``` npm i -g yarn ```
 
+## Configuration
+Create a file named `.env` in the project directory and add the following snippet.
+
+```
+NEO4J_PROTOCOL=neo4j
+NEO4J_HOST=localhost
+NEO4J_USERNAME=neo4j
+NEO4J_PASSWORD=neo4j
+NEO4J_PORT=7687
+NEO4J_DATABASE=neo4j
+NEO4J_ENCRYPTION=ENCRYPTION_OFF
+SERVER_PORT=3001
+```
 ## Project setup
-```
-yarn install
-```
+
+``` yarn install ```
 
 ### Compiles and hot-reloads for development
-```
-yarn serve
-```
+``` yarn serve ```
 
 ### Compiles and minifies for production
-```
-yarn build
-```
+``` yarn build ```
 
 ### Run your unit tests
-```
-yarn test:unit
-```
+``` yarn test:unit ```
 
 ### Lints and fixes files
-```
-yarn lint
-```
+``` yarn lint ```
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/main.js
+++ src/main.js
@@ -3,42 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import VueApollo from "vue-apollo";
-import ApolloClient from "apollo-client";
-import { HttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
 
 Vue.config.productionTip = false;
-Vue.use(VueApollo);
 
-const getHeaders = () => {
-  const headers = {};
-   const token = window.localStorage.getItem('apollo-token');
-   if (token) {
-     headers.authorization = `Bearer ${token}`;
-   }
-   return headers;
- };
- // Create an http link:
- const link = new HttpLink({
-   uri: 'https://hasura.io/learn/graphql',
-   fetch,
-   headers: getHeaders()
- });
- const client = new ApolloClient({
-   link: link,
-   cache: new InMemoryCache({
-     addTypename: true
-   })
- });
-
-const apolloProvider = new VueApollo({
-    defaultClient: client,
-  })
 
 new Vue({
   router,
   store,
-  apolloProvider,
   render: h => h(App)
 }).$mount("#app");

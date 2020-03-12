56c02eb32d1319f657238a64ec3495010352b1a9
Natnael Getahun <connect@ngetahun.me>
2020-03-12 13:18:19 +0100

    Setting up neo4j connection

--- server.js
+++ server.js
@@ -3,21 +3,43 @@ const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const methodOverride = require("method-override");
+const errorHandler = require('errorhandler');
+
+const neo4jSessionCleanup = require('./server/middlewares/neo4jSessionCleanup')
 dotenv.config();
 
-const app = express();
-app.use(
+const api = express();
+api.use(
   bodyParser.urlencoded({
     extended: true
   })
 );
-app.use(bodyParser.json());
-app.use(cookieParser);
-app.use(methodOverride);
+api.use(bodyParser.json());
+api.use(cookieParser());
+api.use(methodOverride());
+api.use(errorHandler());
 
-app.get("/", function(req, res) {
+// CORS setting
+api.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
+api.use(neo4jSessionCleanup);
+
+api.get("/", function(req, res) {
   res.send("Hello World");
 });
 
-app.listen(process.env.SERVER_PORT, "localhost");
-console.log(`[ OK ] Listening on port ${process.env.SERVER_PORT}`);
+api.listen(process.env.SERVER_PORT, "localhost", () => {
+  console.log(`[ OK ] Listening on port ${process.env.SERVER_PORT}`);
+});

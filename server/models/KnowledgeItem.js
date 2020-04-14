73ea7abcd50090130a001cdecaabfcf1f6eaf52b
Natnael Getahun <connect@ngetahun.me>
2020-04-14 16:12:57 +0200

    Lint files

--- server/models/KnowledgeItem.js
+++ server/models/KnowledgeItem.js
@@ -1,31 +1,31 @@
 module.exports = {
-	ID: {
-        type: 'number',
-        primary: true,
-	},
-	Label: 'string',
-	Topic: 'string',
-	Statement: {
-		type: 'string',
-		optional: true,
-		allow: [""]
-		// empty: true,
-		// required: false
-	},
-	ExamBoard: {
-		type: 'string',
-		// empty: true,
-		optional: true,
-		allow: [""]
-		// required: false,
-	},
-	relates_to: {
-		type: 'relationship',
-		target: 'KnowledgeItem',
-        relationship: 'RELTYPE',
-        // direction: 'out',
-        properties: {
-			Type: 'string'
-        },
-	}
-}
+  ID: {
+    type: "number",
+    primary: true
+  },
+  Label: "string",
+  Topic: "string",
+  Statement: {
+    type: "string",
+    optional: true,
+    allow: [""]
+    // empty: true,
+    // required: false
+  },
+  ExamBoard: {
+    type: "string",
+    // empty: true,
+    optional: true,
+    allow: [""]
+    // required: false,
+  },
+  relates_to: {
+    type: "relationship",
+    target: "KnowledgeItem",
+    relationship: "RELTYPE",
+    // direction: 'out',
+    properties: {
+      Type: "string"
+    }
+  }
+};

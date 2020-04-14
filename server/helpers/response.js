aa5b43bf3d3a26901a0bc7835e7386a9602746be
Natnael Getahun <connect@ngetahun.me>
2020-04-14 16:10:19 +0200

    Add logging and configuration

--- server/helpers/response.js
+++ server/helpers/response.js
@@ -1,4 +1,16 @@
+var sw = require("swagger-node-express");
+var _ = require("lodash");
+var logger=require("../logger");
+
 exports.writeResponse = function writeResponse(res, response, status) {
-	// sw.setHeaders(res);
-	res.status(status || 200).send(JSON.stringify(response));
-  };
+  sw.setHeaders(res);
+  res.status(status || 200).send(JSON.stringify(response));
+};
+
+exports.writeError = function writeError(res, error, status) {
+	sw.setHeaders(res);
+	logger.error(error);
+  res
+    .status(error.status || status || 400)
+    .send(JSON.stringify(_.omit(error, ["status"])));
+};

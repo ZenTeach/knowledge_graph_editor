e7d9086274c66663619cffb42df0e9e69dc0ecc8
Natnael Getahun <connect@ngetahun.me>
2020-04-19 17:27:58 +0200

    Update node editing dialog box

--- src/components/VuetifyDialogBoxComponent.vue
+++ src/components/VuetifyDialogBoxComponent.vue
@@ -1,93 +1,109 @@
-
 <template>
-  <v-row justify="center">
-    <v-dialog v-model="dialog" persistent max-width="600px">
-
-      <v-card>
-        <v-card-title>
-          <span class="headline">User Profile</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field label="Legal first name*" required></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field
-                  label="Legal last name*"
-                  hint="example of persistent helper text"
-                  persistent-hint
-                  required
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field label="Email*" required></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field label="Password*" type="password" required></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-select
-                  :items="['0-17', '18-29', '30-54', '54+']"
-                  label="Age*"
-                  required
-                ></v-select>
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-autocomplete
-                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-                  label="Interests"
-                  multiple
-                ></v-autocomplete>
-              </v-col>
-            </v-row>
-          </v-container>
-          <small>*indicates required field</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
-          <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-row>
+  <v-dialog
+    v-model="dialog"
+    persistent
+    max-width="600px"
+    class="override_defaults"
+    ref="EditDialogBox"
+  >
+    <v-card>
+      <v-card-title>
+        <span class="headline"
+          >Update node data for node : {{ nodeAttributes.label }}</span
+        >
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col cols="6">
+              <v-text-field
+                :placeholder="nodeAttributes.value"
+                v-model="value"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                :placeholder="nodeAttributes.statement"
+                v-model="statement"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                :placeholder="nodeAttributes.topic"
+                v-model="topic"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                :placeholder="nodeAttributes.examboard"
+                v-model="examboard"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" text @click.stop="closeDialogBox()"
+          >Close</v-btn
+        >
+        <v-btn color="blue darken-1" text @click="submit()">Save</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
 export default {
-  name:"VuetifyDialogBoxComponent",
-  data: () => ({
-      value: '',
-      topic: '',
-      statement: '',
-      examboard: '',
-    
-  }),
-  method: {
-    submit() {
-      // make API call
-
-      // instead of API call, do emit with data
-
-      
-      
+  name: "VuetifyDialogBoxComponent",
+  props: {
+    dialogController: {
+      type: Boolean,
+      default: false,
+      required: true
     },
-    clear (){
-     // this.$v.$reset()
-      this.value = ''
-      this.topic = ''
-      this.statement = ''
-      this.examboard = ''
+    nodeAttributes: Object
+  },
+  computed: {
+    dialog() {
+      return this.dialogController;
+    }
+  },
+  data: () => ({
+    value: "",
+    topic: "",
+    statement: "",
+    examboard: ""
+  }),
+  methods: {
+    submit() {
+      const payload = {
+        Label: this.value,
+        Topic: this.topic,
+        Statement: this.statement,
+        ExamBoard: this.examboard
+      };
+
+      console.log(payload);
+      // TODO: Finish this part
+      // api.put(`/api/v1/knowledgeitem/${this.nodeAttributes.id}`, payload);
+    },
+    clear() {
+      // this.$v.$reset()
+      this.value = "";
+      this.topic = "";
+      this.statement = "";
+      this.examboard = "";
+    },
+    closeDialogBox() {
+      this.$refs.EditDialogBox.value = false;
     }
   }
-}
+};
 </script>
 
 <style>
-
+.override_defaults {
+  display: block;
+}
 </style>
